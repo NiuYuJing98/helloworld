9bd758a7009919fa93d51ed05a00de3f934845a2
de <de@windows10.microdone.cn>
2018-12-07 17:12:32 +0800

    test

--- com.nyj.src/helloworld/helloworld.java
+++ com.nyj.src/helloworld/helloworld.java
@@ -3,6 +3,6 @@ package helloworld;
 public class helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("hello worldËæ±ã¸ÃÒ»ÏÂ");
+		System.out.println("hello worldéšä¾¿ä¸€è¯´");
 	}
 }
